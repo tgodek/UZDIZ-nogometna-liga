67e782a3431cc4602ab3b9e63b3f560d23d7f7ad
Tomislav Godek <tomislav8462@gmail.com>
2022-01-12 11:51:28 +0100

    Promjenjen namespace

---
+++ tgodek_zadaca_2/Visitor/GetTablicaStrijelci.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tgodek_zadaca_2.Composite;
+
+namespace tgodek_zadaca_2.Visitor
+{
+    class GetTablicaStrijelci : Statistika<int>
+    {
+        private int _kolo;
+
+        public GetTablicaStrijelci(int kolo = 0)
+        {
+            Rezultat = 0;
+            _kolo = kolo;
+        }
+
+        public override void Visit(Dogadaj dogadaj)
+        {
+            if (_kolo == 0)
+            {
+                if (dogadaj.Vrsta == 1 || dogadaj.Vrsta == 2)
+                { 
+                    dogadaj.Igrac.BrojPogodaka++;
+                    Rezultat += 1;
+                }
+            }
+            else
+            {
+                if ((dogadaj.Vrsta == 1 || dogadaj.Vrsta == 2) && dogadaj.Utakmica.Kolo <= _kolo)
+                {
+                    dogadaj.Igrac.BrojPogodaka++;
+                    Rezultat += 1;
+                }
+            }
+        }
+
+        public override void Visit(Igrac igrac)
+        {
+        }
+
+        public override void Visit(Klub klub)
+        {
+        }
+
+        public override void Visit(SastavUtakmice sastav)
+        {
+        }
+
+        public override void Visit(Trener trener)
+        {
+        }
+
+        public override void Visit(Utakmica utakmica)
+        {
+        }
+    }
+}
